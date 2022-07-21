ee44f654148b90d44367e11b2ef50427f9430c58
Tony <ga478267@gmail.com>
2022-07-21 12:43:30 +0800

    新增project

---
+++ covidpeople/src/main/java/com/leedong/covidpeople/CovidpeopleApplication.java
@@ -0,0 +1,13 @@
+package com.leedong.covidpeople;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CovidpeopleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CovidpeopleApplication.class, args);
+    }
+
+}
---
+++ covidpeople/src/main/java/com/leedong/covidpeople/model/CovidPeoples.java
@@ -0,0 +1,16 @@
+package com.leedong.covidpeople.model;
+
+import java.util.List;
+
+public class CovidPeoples {
+    private List<CovidPeople> covidPeopleList;
+
+    public List<CovidPeople> getCovidPeopleList() {
+        return covidPeopleList;
+    }
+
+    public void setCovidPeopleList(List<CovidPeople> covidPeopleList) {
+        this.covidPeopleList = covidPeopleList;
+    }
+}
+
